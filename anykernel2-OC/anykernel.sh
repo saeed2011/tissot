cb0cf55d9345dd524f29c2f9695c2326c35d51ed
AlekDevOfficial <alekdevstream@gmail.com>
2018-10-06 17:40:14 +0200

    Add Treble Support,Zip for FLash,Blank Spectrum Profiles

---
+++ anykernel2-OC/anykernel.sh
@@ -0,0 +1,46 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=eXtremeKernel V12 by X-Team
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=tissot
+device.name2=Mi A1
+device.name3=tissot_sprout
+} # end properties
+
+# shell variables
+block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+#Add Spectrum
+insert_line init.rc "import /init.spectrum.rc" after "import /init.trace.rc" "import /init.spectrum.rc";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
